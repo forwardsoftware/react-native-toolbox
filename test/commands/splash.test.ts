64cf376413eae1aac6d464a41ebdef09cd682d99
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2025-04-30 22:15:28 +0000

    test: enhance `splash` command tests with file generation validation
    - Added tests to verify the generation of iOS and Android splashscreen files when running the `splash` command with an app name.
    - Enhanced test assertions to check for the existence and count of generated splashscreen files in the respective output directories.
    - Implemented cleanup of generated directories after each test to ensure isolation.

--- test/commands/splash.test.ts
+++ test/commands/splash.test.ts
@@ -1,17 +1,50 @@
-import { runCommand } from '@oclif/test'
-import { expect } from 'chai'
+import {runCommand} from '@oclif/test'
+import {expect} from 'chai'
+import fs from 'node:fs'
+import path from 'node:path'
+import {rimrafSync} from 'rimraf'
 
 describe('splash', () => {
+  afterEach(() => {
+    rimrafSync(['android', 'ios'])
+  })
+
   it('should fail to run splash when no app.json file exists', async () => {
-    const { error } = await runCommand(['splash'])
+    const {error} = await runCommand(['splash'])
 
     expect(error?.oclif?.exit).to.equal(2)
   })
 
-
-  it('runs splash --appName test', async () => {
-    const { stdout } = await runCommand(['splash', '--appName', 'test'])
+  it('runs splash --appName test and generates expected files', async () => {
+    const {stdout} = await runCommand(['splash', '--appName', 'test'])
 
     expect(stdout).to.contain("Generating splashscreens for 'test' app...")
+    expect(stdout).to.contain("Generated splashscreens for 'test' app.")
+
+    // Check for iOS output directory and Contents.json
+    const iosDir = path.join('ios', 'test', 'Images.xcassets', 'Splashscreen.imageset')
+    expect(fs.existsSync(path.join(iosDir, 'Contents.json'))).to.be.true
+
+    // Check for iOS image files (expecting at least one)
+    const iosSplashImages = fs.readdirSync(iosDir).filter((f) => f.endsWith('.png'))
+    expect(iosSplashImages.length).to.eq(3)
+
+    // Check for Android output directories and image files
+    const androidDir = path.join('android', 'app', 'src', 'main', 'res')
+    expect(fs.existsSync(androidDir)).to.be.true
+
+    // Count drawable directories
+    const drawableDirs = fs.readdirSync(androidDir).filter((f) => f.startsWith('drawable-'))
+    expect(drawableDirs.length).to.eq(6)
+
+    // Count icons in drawable directories
+    let androidSplashscreensCount = 0
+    for (const drawableDir of drawableDirs) {
+      const pngFilesInDir = fs.readdirSync(path.join(androidDir, drawableDir)).filter((f) => f.endsWith('.png'))
+      androidSplashscreensCount += pngFilesInDir.length
+    }
+
+    // Expect 6 densities * 1 splashscreen = 6
+    expect(androidSplashscreensCount).to.eq(6)
   })
 })
