3225ee8ce551696c4131903d2a09ea43236b5d47
Mattia Panzeri <1754457+panz3r@users.noreply.github.com>
2026-01-10 23:46:00 +0100

    chore(tests): add copyright header to `dotenv`, `icons`, and `splash` test files

--- test/commands/icons.test.ts
+++ test/commands/icons.test.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2025 ForWarD Software (https://forwardsoftware.solutions/)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import {expect} from 'chai'
 import fs from 'node:fs'
 import path from 'node:path'
